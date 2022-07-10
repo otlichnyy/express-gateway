77c672be7880512dd5be8a780635dfd4cdb41d68
otlichnny <12508523+otlichnyy@users.noreply.github.com>
2022-07-10 21:18:05 +0530

    feat: :sparkles: add supertoken support to express server
    automatic handle all routes at /auth, provide session management using verifySession middleware

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,20 @@ import helmet from 'helmet';
 import cors from 'cors';
 import dotenv from 'dotenv-safe';
 import httpStatus from 'http-status';
+import supertokens from 'supertokens-node';
+import {
+  middleware,
+  errorHandler as stErrorHandler,
+} from 'supertokens-node/framework/express';
 import routes from '@src/routes/v1';
 import { ErrorMiddleware, rateLimiter } from '@src/middleware';
 import { successHandler, errorHandler } from '@src/config/morgan';
 import ApiError from '@src/utils/ApiError';
+import backendConfig, { appDomain } from '@src/config/supertokens';
 
 dotenv.config();
+
+supertokens.init(backendConfig());
 
 const app = express();
 
@@ -18,8 +26,24 @@ if (process.env.NODE_ENV !== 'test') {
   app.use(errorHandler);
 }
 
+// enable cors
+app.use(
+  cors({
+    origin: appDomain,
+    allowedHeaders: ['content-type', ...supertokens.getAllCORSHeaders()],
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    credentials: true,
+  })
+);
+
 // set security HTTP headers
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+
+app.use(middleware());
 
 // parse json request body
 app.use(express.json());
@@ -27,21 +51,21 @@ app.use(express.json());
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
 
-// enable cors
-app.use(cors());
-
 // v1 api routes
 app.use('/v1', rateLimiter, routes);
 
-// // send back a 404 error for any unknown api request
+// send back a 404 error for any unknown api request
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
 
-// // convert error to ApiError, if needed
+// supertokens error handler
+app.use(stErrorHandler());
+
+// convert error to ApiError, if needed
 app.use(ErrorMiddleware.converter);
 
-// // handle error
+// handle error
 app.use(ErrorMiddleware.handler);
 
 export default app;
